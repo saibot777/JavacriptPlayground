4e247f709a881f419474befdcaabd242d5c59e3a
saibot777 <stefantrajkovic777@gmail.com>
2018-01-03 13:21:46 +0100

    category-theory upd

--- functional-paradigm/patterns/category-theory.js
+++ functional-paradigm/patterns/category-theory.js
@@ -1,12 +1,24 @@
-let str = (s) => {
-    if (typeof s !== "string") {
-        throw new TypeError("Expected a string!");
-    } else {
-        return s;
-    }
-};
+let typeOf = (type) => {
+    type = str(type);
+    return (p) => {
+        if (typeof p !== type) {
+            throw new TypeError("Expected a " + type + "!");
+        } else {
+            return p;
+        }
+    };
+}
 
-let repeat  = (s) => {
-    s = str(s);
-    return s + s;
-};
+let bool = typeOf("boolean");
+let obj = typeOf("object");
+let num = typeOf("number");
+let undef = typeOf("undefined");
+
+let inc = (x) => {
+    x = num(x); 
+    return num(x + 1);
+}
+
+ 
+let any = (x) => x;  // <-- if not using categories 
+
