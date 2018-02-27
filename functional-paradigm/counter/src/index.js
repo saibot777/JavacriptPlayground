2984a6beaa5da6c95f397652195975d72ff3463b
saibot777 <stefantrajkovic777@gmail.com>
2018-02-27 12:59:14 +0100

    update counter and app function implemented

--- functional-paradigm/counter/src/index.js
+++ functional-paradigm/counter/src/index.js
@@ -1,29 +1,55 @@
-import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
+import { h, diff, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
 const { div, button } = hh(h);
 
 const initModel = 0;
 
-function view(model) {
-    return div([
-        div({ className: 'mv2' }, `Count: ${model}`),
-        button({ className: 'pv1 ph2 mr2', onclick: () => console.log('+ clicked!') }, '+'),
-        button({ className: 'pv1 ph2', onclick: () => console.log('- clicked!') }, '-'),
-    ]);
+function view(dispatch, model) {
+  return div([
+    div({ className: 'mv2' }, `Count: ${model}`),
+    button({ className: 'pv1 ph2 mr2', 
+      onclick: () => dispatch(MSGS.ADD) }, '+'),
+    button({ className: 'pv1 ph2',
+      onclick: () => dispatch(MSGS.SUBTRACT) }, '-'),
+  ]);
 }
 
+const MSGS = {
+  ADD: 'ADD',
+  SUBTRACT: 'SUBTRACT',
+};
+
 function update(msg, model) {
-    switch (msg) {
-        case 'plus' : 
-            return model + 1;
-        case 'minus' : 
-            return model - 1;  
-        default : 
-            return model;      
-    }
+  switch (msg) {
+    case MSGS.ADD:
+      return model + 1;
+    case MSGS.SUBTRACT: 
+      return model - 1;
+    default:
+      return model;
+  }
 }
+
+// impure code below
+
+function app(initModel, update, view, node) {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+  let rootNode = createElement(currentView);
+  node.appendChild(rootNode);
+  function dispatch(msg){
+    model = update(msg, model);
+    const updatedView = view(dispatch, model);
+    const patches = diff(currentView, updatedView);
+    rootNode = patch(rootNode, patches);
+    currentView = updatedView;
+  }
+} 
 
 const rootNode = document.getElementById('app');
 
-rootNode.appendChild(view(update('plus', initModel)));
+app(initModel, update, view, rootNode);
+
+// rootNode.appendChild(view(update('minus', initModel)));
