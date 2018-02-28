616df484d47e2978928c3dbbeaf3b452175a8762
saibot777 <stefantrajkovic777@gmail.com>
2018-02-28 16:39:55 +0100

    view and formView UI implemented

--- functional-paradigm/caloriecounter/src/View.js
+++ functional-paradigm/caloriecounter/src/View.js
@@ -1,10 +1,22 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
 
-const { pre } = hh(h);
+const { pre, div, h1, button } = hh(h);
+
+function formView(dispatch, model) {
+    return button(
+        { className: 'f3 pv2 ph3 bg-blue white bn' },
+        'Add Meal'
+    )
+}
 
 function view(dispatch, model) {
-    return pre(JSON.stringify(model, null, 2));
+    return div({ className: 'mw6 center' }, [
+        h1({ className: 'f2 pv2 bb'}, 'CalorieCounter' ),
+        formView(),
+        pre(JSON.stringify(model, null, 2))
+    ])
+    
 }
 
 export default view;
