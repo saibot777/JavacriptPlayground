fb8eb0f30fa3c6ba8bbe71c32aa6adb7008c39d9
saibot777 <stefantrajkovic777@gmail.com>
2018-03-01 13:10:54 +0100

    edit func implmemented

--- functional-paradigm/caloriecounter/src/Update.js
+++ functional-paradigm/caloriecounter/src/Update.js
@@ -5,7 +5,8 @@ const MSGS = {
     MEAL_INPUT: 'MEAL_INPUT',
     CALORIES_INPUT: 'CALORIES_INPUT',
     SAVE_MEAL: 'SAVE_MEAL',
-    DELETE_MEAL: 'DELETE_MEAL'
+    DELETE_MEAL: 'DELETE_MEAL',
+    EDIT_MEAL: 'EDIT_MEAL'
 };
 
 export function showFormMsg(showForm) {
@@ -38,6 +39,13 @@ export function deleteMealMsg(id) {
     }
 }
 
+export function editMealMsg(editId) {
+    return {
+        type: MSGS.EDIT_MEAL,
+        editId
+    }
+}
+
 function update(msg, model) {
     switch (msg.type) {
         case MSGS.SHOW_FORM: {
@@ -56,7 +64,28 @@ function update(msg, model) {
             return { ...model, calories };
         }
         case MSGS.SAVE_MEAL: {
-            return add(msg, model);
+            const { editId } = model;
+            const updatedModel = editId !== null 
+                ? edit(msg, model) 
+                : add(msg, model);
+            return updatedModel;
+        }
+        case MSGS.EDIT_MEAL: {
+            const { editId } = msg;
+            const meal = R.find(
+                meal => meal.id === editId,
+                model.meals
+            );
+
+            const { description, calories } = meal;
+
+            return { 
+                ...model, 
+                editId,
+                description,
+                calories,
+                showForm: true
+            }; 
         }
         case MSGS.DELETE_MEAL: {
             const { id } = msg;
@@ -82,6 +111,24 @@ function add(msg, model) {
         calories: 0,
         showForm: false
     };
+}
+
+function edit(msg, model) {
+    const { description, calories, editId } = model;
+    const meals = R.map(meal => {
+        if (meal.id === editId) {
+            return { ...meal, description, calories };
+        }
+        return meal;
+    }, model.meals);
+    return {
+        ...model,
+        meals,
+        description: '',
+        calories: 0,
+        showForm: false,
+        editId: null
+    }
 }
 
 export default update;
